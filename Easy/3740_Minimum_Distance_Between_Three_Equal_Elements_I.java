539d16368496ed2e16685ddf8a0f441ec89db1b8
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-04-11 00:08:31 +0530

    Update 3740_Minimum_Distance_Between_Three_Equal_Elements_I.java

--- Easy/3740_Minimum_Distance_Between_Three_Equal_Elements_I.java
+++ Easy/3740_Minimum_Distance_Between_Three_Equal_Elements_I.java
@@ -1,6 +1,7 @@
 /*
 LeetCode Link: https://leetcode.com/problems/minimum-distance-between-three-equal-elements-i/?envType=daily-question&envId=2026-04-10
 */
+//Brute Force Approach
 class Solution {
     public int minimumDistance(int[] nums) {
         int n = nums.length;
@@ -28,6 +29,31 @@ class Solution {
                 int d = Math.abs(i - j) + Math.abs(j - k) + Math.abs(k - i);
                 if(d < min)
                 min = d;
+            }
+        }
+        if(min != Integer.MAX_VALUE)
+        return min;
+        return -1;
+    }
+}
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//Optimal Solution
+class Solution {
+    public int minimumDistance(int[] nums) {
+        HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
+        int n = nums.length;
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i<n; i++)
+        {
+            map.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
+            
+            ArrayList<Integer> list = map.get(nums[i]);
+            if(list.size() >= 3)
+            {
+                int k = list.get(list.size()-1);
+                int p = list.get(list.size()-3);
+                min = Math.min(min, 2 * (k - p));
             }
         }
         if(min != Integer.MAX_VALUE)
