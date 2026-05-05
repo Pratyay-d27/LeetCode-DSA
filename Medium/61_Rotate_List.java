127e798ae83cd7457a752b0762d230382ad1a8df
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-05-05 16:26:59 +0530

    Create 61_Rotate_List.java

---
+++ Medium/61_Rotate_List.java
@@ -0,0 +1,37 @@
+/*
+LeetCode Link: https://leetcode.com/problems/rotate-list/?envType=daily-question&envId=2026-05-05
+*/
+
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        //count length of linked list
+        int n = 0;
+        ListNode ptr = head;
+        while(ptr != null)
+        {
+            n++;
+            ptr = ptr.next;
+        }
+        //now n = length of linked list
+        //base case 
+        if(n<2)
+        return head;
+
+        //rest of the code 
+        k = k % n;
+        while( k > 0)
+        {
+            ListNode temp = head;
+            while(temp.next.next != null)
+            temp = temp.next;
+            //now temp is the 2nd last node 
+            
+            temp.next.next = head;
+            head = temp.next;
+            temp.next = null;
+            //decrement k
+            k--;
+        }
+        return head;
+    }
+}
