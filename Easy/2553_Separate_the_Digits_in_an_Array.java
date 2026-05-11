4f3c40b1214110e8bcb817e3f33c9d2b76f3ff26
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-05-11 12:19:16 +0530

    Create 2553_Separate_the_Digits_in_an_Array.java

---
+++ Easy/2553_Separate_the_Digits_in_an_Array.java
@@ -0,0 +1,20 @@
+/*
+LeetCode Link: https://leetcode.com/problems/separate-the-digits-in-an-array/?envType=daily-question&envId=2026-05-11
+*/
+
+class Solution {
+    public int[] separateDigits(int[] nums) {
+        String s = "";
+        for(int ele: nums)
+        s += String.valueOf(ele);
+        int n = s.length();
+        int arr[] = new int[n];
+        int i = 0;
+        for(char ch: s.toCharArray())
+        {
+            arr[i] = ch - '0';
+            i++;
+        }
+        return arr;
+    }
+}
