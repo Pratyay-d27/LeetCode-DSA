81a6c43c77d045e10ff9c831035eaccb83183c2c
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-05-11 12:34:17 +0530

    Create 1550_Three_Consecutive_Odds.java

---
+++ Easy/1550_Three_Consecutive_Odds.java
@@ -0,0 +1,23 @@
+/*
+LeetCode Link: https://leetcode.com/problems/three-consecutive-odds/
+*/
+
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int n = arr.length;
+        int i, p;
+        p=0;
+        for(i=0;i<n;i++)
+        {
+            if(arr[i] % 2 == 1)
+            {
+                p=p+1;
+            }
+            else
+            p=0;
+            if (p ==3)
+            return true;
+        }
+        return false;
+    }
+}
