de2779760194917ad0891e4ce15fae791fc3e0b8
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-05-12 17:48:46 +0530

    Update 1665_Minimum_Initial_Energy_to_Finish_Tasks.java

--- Hard/1665_Minimum_Initial_Energy_to_Finish_Tasks.java
+++ Hard/1665_Minimum_Initial_Energy_to_Finish_Tasks.java
@@ -7,7 +7,7 @@ class Solution {
         //sort according to difference 
         Arrays.sort(tasks, (a, b) -> Integer.compare((b[1]-b[0]), (a[1]-a[0])));
 
-        //sorting done. Now binary search
+        //sorting done. Now binary search and isPossible function 
         int l = 0, r = (int)Math.pow(10, 9), result = Integer.MAX_VALUE;
         while(l<=r)
         {
