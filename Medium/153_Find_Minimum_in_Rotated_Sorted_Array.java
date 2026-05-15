13fa017cf07aa3220cd3866f3eec6e605dc4fd2f
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-05-15 13:16:19 +0530

    Implement findMin using TreeSet and ArrayList
    Added a new method using TreeSet and ArrayList to find the minimum element in a rotated sorted array.

--- Medium/153_Find_Minimum_in_Rotated_Sorted_Array.java
+++ Medium/153_Find_Minimum_in_Rotated_Sorted_Array.java
@@ -11,3 +11,14 @@ class Solution {
         return min;
     }
 }
+
+//Method 2 -- Using TreeSet and ArrayList
+class Solution {
+    public int findMin(int[] nums) {
+        TreeSet<Integer> set = new TreeSet<>();
+        for(int ele: nums)
+        set.add(ele);
+        ArrayList<Integer> list = new ArrayList<>(set);
+        return list.get(0);
+    }
+}
