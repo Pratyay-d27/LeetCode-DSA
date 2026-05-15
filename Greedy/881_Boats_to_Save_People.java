f5103e5e44a4b5dd1c55658d6d481b54db05e6cf
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-05-15 16:01:13 +0530

    Create 881_Boats_to_Save_People.java

---
+++ Greedy/881_Boats_to_Save_People.java
@@ -0,0 +1,29 @@
+/*
+LeetCode Link: https://leetcode.com/problems/boats-to-save-people/
+*/
+
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int count = 0;
+        int i = 0, j = people.length-1;
+        while(i<=j)
+        {
+            if(people[i]<=limit && people[j]<=limit)
+            {
+                if(people[i]+people[j] <= limit)
+                {
+                    count++;
+                    i++;
+                    j--;
+                    continue;
+                }
+            }
+            count++;
+            j--;
+        }
+        return count;
+    }
+}
+
+//1 2 2 3
