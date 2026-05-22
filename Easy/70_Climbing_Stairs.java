7fd40dc252cc340d4bf3daa4c1522f49206be1d8
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-05-23 00:45:44 +0530

    Update 70_Climbing_Stairs.java

--- Easy/70_Climbing_Stairs.java
+++ Easy/70_Climbing_Stairs.java
@@ -27,6 +27,7 @@ class Solution {
     public int climbStairs(int n) {
         int t[] = new int[46];
         t[0] = 0;t[1] = 1;t[2] = 2;
+        //base case 
         if(n<2)
         return t[n];
         for(int i = 3; i<=n; i++)
