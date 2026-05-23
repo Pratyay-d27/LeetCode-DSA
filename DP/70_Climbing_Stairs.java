dbc67943ee90d170ab2eaf964a8dd2d6aaa77b4f
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-05-23 15:30:49 +0530

    Create 70_Climbing_Stairs.java

---
+++ DP/70_Climbing_Stairs.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int climbStairs(int n) {
+        int t[] = new int[46];
+        t[0] = 0;t[1] = 1;t[2] = 2;
+        if(n<2)
+        return t[n];
+        for(int i = 3; i<=n; i++)
+        t[i] = t[i-1] + t[i-2];
+        return t[n];
+    }
+}
