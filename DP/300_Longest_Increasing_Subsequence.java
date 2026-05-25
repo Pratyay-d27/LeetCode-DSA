43003a03e9c678b88f121b6d7f7731416e01d36a
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-05-25 16:07:59 +0530

    Create 300_Longest_Increasing_Subsequence.java

---
+++ DP/300_Longest_Increasing_Subsequence.java
@@ -0,0 +1,26 @@
+class Solution {
+    int t[][] = new int[2501][2501];
+    public int lengthOfLIS(int[] nums) {
+        return solve(nums, 0, -1);
+    }
+    int solve(int nums[], int i, int p)
+    {
+        //initially i = 0, p = -1
+        //base case
+        if(i>=nums.length)
+        return 0;
+
+        //memoize 
+        if(p>-1 && t[i][p] != 0)
+        return t[i][p];
+
+        int take = 0, skip = 0;
+        if(p == -1 || nums[p] < nums[i])
+        take = solve(nums, i+1, i) + 1;
+        skip = solve(nums, i+1, p);
+
+        if(p == -1)
+        return Math.max(take, skip);
+        return t[i][p] = Math.max(take, skip);
+    }
+}
