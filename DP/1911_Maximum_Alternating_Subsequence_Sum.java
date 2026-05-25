246111a07c5810856d11b0bf775307c5398bfdbe
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-05-25 13:27:21 +0530

    Add bottom-up DP solution for max alternating sum
    Implemented a bottom-up dynamic programming solution to calculate the maximum alternating subsequence sum.

--- DP/1911_Maximum_Alternating_Subsequence_Sum.java
+++ DP/1911_Maximum_Alternating_Subsequence_Sum.java
@@ -25,3 +25,15 @@ class Solution {
 }
 
 //Bottom-Up DP Solution
+class Solution {
+    public long maxAlternatingSum(int[] nums) {
+        int n = nums.length;
+        long dp[][] = new long[n+1][2];
+        for(int i = 1; i<=n; i++)
+        {//0 = even, 1 = odd
+            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] - nums[i-1]);
+            dp[i][1] = Math.max(dp[i-1][1], dp[i-1][0] + nums[i-1]);
+        }
+        return Math.max(dp[n][0], dp[n][1]);
+    }
+}
