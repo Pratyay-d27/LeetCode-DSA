895492480897e805a9573015289c12ea19151652
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-05-28 00:25:01 +0530

    Update 300_Longest_Increasing_Subsequence.java

--- DP/300_Longest_Increasing_Subsequence.java
+++ DP/300_Longest_Increasing_Subsequence.java
@@ -25,3 +25,26 @@ class Solution {
         return t[i][p] = Math.max(take, skip);
     }
 }
+
+//Bottom Up Soltuion
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int t[] = new int[n];
+        Arrays.fill(t, 1);
+        int max = 1;
+        for(int i = 0; i<n; i++)
+        {
+            for(int j = 0; j<i; j++)
+            {
+                if(nums[j] < nums[i])
+                {
+                    t[i] = Math.max(t[i], t[j]+1);
+                    max = Math.max(max, t[i]);
+                }
+            }
+        }
+
+        return max;
+    }
+}
