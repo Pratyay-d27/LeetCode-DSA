562a4654b53db1df1e51096cc106f3d6fd990ccd
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-06-08 19:32:24 +0530

    Create 2161_Partition_Array_According_to_Given_Pivot.java

---
+++ 2. Medium/2161_Partition_Array_According_to_Given_Pivot.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] pivotArray(int[] nums, int pivot) {
+        int n = nums.length, index = 0, equal = 0;
+        int arr[] = new int[n];
+        for(int i = 0; i<n; i++)
+        {//all elements less than pivot 
+            if(nums[i] < pivot)
+            {
+                arr[index++] = nums[i];
+                nums[i] = 1000001;
+            }
+            if(nums[i] == pivot)
+            {
+                nums[i] = 1000001;
+                equal++;
+            }
+        }
+        for(int i = 0; i<equal; i++)
+        {
+            arr[index++] = pivot;
+        }
+        for(int i = 0; i<n; i++)
+        {
+            if(nums[i] != 1000001)
+            arr[index++] = nums[i];
+        }
+        return arr;
+    }
+}
