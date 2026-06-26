53bf344b8d1502047640775a4b59e7421309a9b1
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-06-26 15:52:33 +0530

    Update 153_Find_Minimum_in_Rotated_Sorted_Array.java

--- 2. Medium/153_Find_Minimum_in_Rotated_Sorted_Array.java
+++ 2. Medium/153_Find_Minimum_in_Rotated_Sorted_Array.java
@@ -2,7 +2,7 @@
 LeetCode Link: https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/?envType=daily-question&envId=2026-05-15
 */
 
-//Method 1
+//Method 1 -- usijg brute force 
 class Solution {
     public int findMin(int[] nums) {
         int min = Integer.MAX_VALUE;
