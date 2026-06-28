e06ebd255218689c478eb036b617d9a0d7a65a73
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-06-28 14:20:25 +0530

    Update 461_Hamming_Distance.java

--- 1. Easy/461_Hamming_Distance.java
+++ 1. Easy/461_Hamming_Distance.java
@@ -5,9 +5,11 @@ LeetCode Link: https://leetcode.com/problems/hamming-distance/
 class Solution {
     public int hammingDistance(int x, int y) {
         int i, j, count = 0;
+        
         //base case
         if(x == 0 && y == 0)
         return 0;
+        
         //rest of the code 
         while(x>0 || y>0)
         {
