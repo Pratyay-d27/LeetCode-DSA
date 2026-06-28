2e368dd39fdfbbdb2da367b37505c99d5a1ba79c
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-06-28 14:37:00 +0530

    Fix first element assignment in maximumElement method
    Ensure the first element is set to 1 if not already, following problem constraints.

--- 2. Medium/1846_Maximum_Element_After_Decreasing_and_Rearranging.java
+++ 2. Medium/1846_Maximum_Element_After_Decreasing_and_Rearranging.java
@@ -1,13 +1,16 @@
 class Solution {
     public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
         Arrays.sort(arr);//sorting the array
-        if(arr[0] != 1)
+        
+        if(arr[0] != 1)//if first element is not 1, then making it = 1 as per the question guidelines 
         arr[0] = 1;
+        
         for(int i = 1; i<arr.length; i++)
         {
             if(Math.abs(arr[i] - arr[i-1]) >1)
             arr[i] = 1 + arr[i-1];
         }
+        
         Arrays.sort(arr);
         return arr[arr.length-1];
     }
