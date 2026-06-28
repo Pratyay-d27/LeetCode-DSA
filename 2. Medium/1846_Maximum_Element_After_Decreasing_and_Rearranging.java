468f7141d7cbc31fae9822dd1418ded005918f17
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-06-28 14:36:15 +0530

    Update 1846_Maximum_Element_After_Decreasing_and_Rearranging.java

--- 2. Medium/1846_Maximum_Element_After_Decreasing_and_Rearranging.java
+++ 2. Medium/1846_Maximum_Element_After_Decreasing_and_Rearranging.java
@@ -1,6 +1,6 @@
 class Solution {
     public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
-        Arrays.sort(arr);
+        Arrays.sort(arr);//sorting the array
         if(arr[0] != 1)
         arr[0] = 1;
         for(int i = 1; i<arr.length; i++)
