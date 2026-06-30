4ca678b47ee98e9b145a8470d55029de5f03be78
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-06-30 21:28:36 +0530

    Add comment to sum variable in sumOfGoodIntegers
    Added comment for clarity on the sum variable.

--- 1. Easy/3954_Sum_of_Compatible_Numbers_in_Range_I.java
+++ 1. Easy/3954_Sum_of_Compatible_Numbers_in_Range_I.java
@@ -1,6 +1,6 @@
 class Solution {
     public int sumOfGoodIntegers(int n, int k) {
-        int sum = 0;
+        int sum = 0; //variable for storing the final sum
         for(int i = 1; i<= (n+k); i++)
         {
             if(Math.abs(i-n) <= k && ((i & n) == 0))
