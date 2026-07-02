af9bdc8d0952efeebc5382db5c7be3bf839997cf
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-07-02 23:02:55 +0530

    Update 2833_Furthest_Point_From_Origin.java

--- 1. Easy/2833_Furthest_Point_From_Origin.java
+++ 1. Easy/2833_Furthest_Point_From_Origin.java
@@ -14,6 +14,6 @@ class Solution {
             else
             space++;
         }
-       return Math.abs(left - right) + space;
+       return Math.abs(left - right) + space; //return absolute value 
     }
 }
