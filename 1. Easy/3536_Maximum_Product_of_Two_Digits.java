ec63b650c3f45ea43f36e4bf7b58f295309a3085
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-07-25 12:34:37 +0530

    Implement maxProduct method to find max digit product

---
+++ 1. Easy/3536_Maximum_Product_of_Two_Digits.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProduct(int n) {
+        if(n < 100)
+        return (n%10) * (n/10);
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        while(n>0)
+        {
+            list.add(n%10);
+            n = n/10;
+        }
+        Collections.sort(list);
+        return list.get(list.size()-1) * list.get(list.size()-2);
+    }
+}
