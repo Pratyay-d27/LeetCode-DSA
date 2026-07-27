6cb9351ef1a5554fc9e0ac1b9aafaf799f6d0cf4
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-07-27 14:02:20 +0530

    Create 4000_Largest_Integer_With_Given_Digit_Sum.java

---
+++ 1. Easy/4000_Largest_Integer_With_Given_Digit_Sum.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int largestInteger(int n, int s) {
+        if(s == 0)
+        return 0;
+
+        int dig = (int) Math.pow(10, n) - 1;
+        if(sum(dig) < s)
+        return -1;
+        while(dig >= 0)
+        {
+            if(sum(dig) == s)
+            return dig;
+            dig--;
+        }
+        return -1;
+    }
+    int sum(int n)
+    {
+        int ans = 0;
+        while(n > 0)
+        {
+            int d = n % 10;
+            ans += d;
+            n = n / 10;
+        }
+        return ans;
+    }
+}
