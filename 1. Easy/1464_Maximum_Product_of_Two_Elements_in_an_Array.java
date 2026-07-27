e30f7afdda75fe3153de8480442a3a6803c8f80c
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-07-27 13:51:55 +0530

    Create 1464_Maximum_Product_of_Two_Elements_in_an_Array.java

---
+++ 1. Easy/1464_Maximum_Product_of_Two_Elements_in_an_Array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE;
+        for(int ele: nums)
+        {
+            if(ele > max1)
+            {
+                max2 = max1;
+                max1 = ele;
+            }
+            else if(ele > max2)
+            max2 = ele;
+        }
+        return (max1-1) * (max2-1);
+    }
+}
