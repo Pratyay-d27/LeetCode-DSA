04c98542331af40ecc7a85f3fe106685a75b5294
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-07-29 13:52:57 +0530

    Update 1078_Occurrences_After_Bigram.java

--- 1. Easy/1078_Occurrences_After_Bigram.java
+++ 1. Easy/1078_Occurrences_After_Bigram.java
@@ -6,7 +6,7 @@ class Solution {
         if(arr.length <= 2)
         return new String[0];
 
-        //rest of the cocee
+        //rest of the code
         ArrayList<String> list = new ArrayList<>();
         int i = 0, j = 1;
         while(i<arr.length && j<arr.length)
