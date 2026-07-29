18b7afa8de5b85dd614d4c636057487c49e8cb76
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-07-29 13:49:15 +0530

    Update 1078_Occurrences_After_Bigram.java

--- 1. Easy/1078_Occurrences_After_Bigram.java
+++ 1. Easy/1078_Occurrences_After_Bigram.java
@@ -1,6 +1,6 @@
 class Solution {
     public String[] findOcurrences(String text, String first, String second) {
-        String arr[] = text.split(" ");
+        String arr[] = text.split(" "); //
         
         //base case 
         if(arr.length <= 2)
