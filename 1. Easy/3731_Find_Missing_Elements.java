9fd2454978a4721db02d433343f1a9dfaa92eb9d
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-08-04 14:22:41 +0530

    Update 3731_Find_Missing_Elements.java

--- 1. Easy/3731_Find_Missing_Elements.java
+++ 1. Easy/3731_Find_Missing_Elements.java
@@ -2,7 +2,7 @@ class Solution {
     public List<Integer> findMissingElements(int[] nums) {
         int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
         boolean pres[] = new boolean[102];
-        for(int ele: nums)
+        for(int ele: nums) //to
         {
             pres[ele] = true;
             max = Math.max(ele, max);
