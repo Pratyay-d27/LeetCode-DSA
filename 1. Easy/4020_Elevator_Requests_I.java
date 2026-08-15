b2cb892afd98d1e53044a4c586db32e97645599e
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-08-15 22:34:38 +0530

    Update 4020_Elevator_Requests_I.java

--- 1. Easy/4020_Elevator_Requests_I.java
+++ 1. Easy/4020_Elevator_Requests_I.java
@@ -3,7 +3,7 @@ class Solution {
         int present = 0, time = 0;
         for(int toFloor: requests)
             {
-                if(present == toFloor)
+                if(present == toFloor) //if present floor same as the intended floor to go
                     time = time;
                 else if(present != toFloor)
                 {
