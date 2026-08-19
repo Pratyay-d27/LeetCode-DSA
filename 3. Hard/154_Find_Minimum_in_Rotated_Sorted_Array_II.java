92ebcebab43cf68252e8b5ad5bf9230fca81cc7f
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-08-19 23:35:57 +0530

    Update 154_Find_Minimum_in_Rotated_Sorted_Array_II.java

--- 3. Hard/154_Find_Minimum_in_Rotated_Sorted_Array_II.java
+++ 3. Hard/154_Find_Minimum_in_Rotated_Sorted_Array_II.java
@@ -4,7 +4,7 @@ LeetCode Link: https://leetcode.com/problems/find-minimum-in-rotated-sorted-arra
 
 class Solution {
     public int findMin(int[] nums) {
-        TreeSet<Integer> set = new TreeSet<>();
+        TreeSet<Integer> set = new TreeSet<>(); //using treeset for sorted and unduplicated integer storing 
         for(int ele: nums)
         set.add(ele); 
         ArrayList<Integer> list = new ArrayList<>(set);
