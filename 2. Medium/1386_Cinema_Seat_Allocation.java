88f9123a9c866710217de5a9b51baaec01fc3fa9
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-08-20 00:01:25 +0530

    Implement cinema seat allocation solution

---
+++ 2. Medium/1386_Cinema_Seat_Allocation.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
+        HashMap<Integer, ArrayList<Integer>>map = new HashMap<>();
+        for(int i = 0; i<reservedSeats.length; i++)
+        {
+            if(map.containsKey(reservedSeats[i][0]) == false)
+            {
+                ArrayList<Integer> list = new ArrayList<>();
+                list.add(reservedSeats[i][1]);
+                map.put(reservedSeats[i][0], list);
+            }
+            else 
+            {
+                ArrayList<Integer> list = map.get(reservedSeats[i][0]);
+                list.add(reservedSeats[i][1]);
+                map.put(reservedSeats[i][0], list);
+            }
+        }
+        int count = 0;
+        for(var ele: map.entrySet())
+        {
+
+            int i = ele.getKey();
+            ArrayList<Integer> list = map.get(i);
+            if(list.size() >= 7)
+            continue;
+
+            int max = 0;
+
+            if((list.contains(2) || list.contains(3) || list.contains(4) || list.contains(5)) == false )
+            {
+                count++;
+                max = 1;
+                //System.out.println(i + " in 1");
+            }
+
+            if((list.contains(6) || list.contains(7) || list.contains(8) || list.contains(9)) == false )
+            {
+                count++;
+                max = 13;
+                //System.out.println(i + " in 3");
+            }
+
+            if(max == 0)
+            {
+                if((list.contains(4) || list.contains(5) || list.contains(6) || list.contains(7)) == false )
+                {
+                    count++;
+                    //System.out.println(i + " in 2");
+                }
+            }
+        }
+        return count + ((n - map.size()) * 2);
+    }
+}
