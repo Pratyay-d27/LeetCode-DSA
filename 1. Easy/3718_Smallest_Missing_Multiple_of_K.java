55b21140211d649021baf6b0df6f06b22e286803
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-08-25 17:27:25 +0530

    Update 3718_Smallest_Missing_Multiple_of_K.java

--- 1. Easy/3718_Smallest_Missing_Multiple_of_K.java
+++ 1. Easy/3718_Smallest_Missing_Multiple_of_K.java
@@ -12,7 +12,7 @@ class Solution {
             if(freq[key] == 0)
             return key;
         }
-        //
+        // this 
         int num = k;
         while(num <= 100)
             num += k;
