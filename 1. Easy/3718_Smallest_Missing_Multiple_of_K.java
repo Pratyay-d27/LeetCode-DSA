702f2b9a2fd9c03a9ef2e49abe0fc0573277d746
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-08-25 17:25:58 +0530

    Update 3718_Smallest_Missing_Multiple_of_K.java

--- 1. Easy/3718_Smallest_Missing_Multiple_of_K.java
+++ 1. Easy/3718_Smallest_Missing_Multiple_of_K.java
@@ -1,6 +1,6 @@
 class Solution {
     public int missingMultiple(int[] nums, int k) {
-        int freq[] = new int[102]; //declaring 
+        int freq[] = new int[102]; //declaring frequency 
         for(int ele: nums)
         freq[ele] = 1;
 
