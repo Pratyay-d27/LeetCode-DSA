b49033a08838bc880ac5776b5dfd76088d78466b
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-08-27 22:44:50 +0530

    Update 1563_Stone_Game_V.java

--- 3. Hard/1563_Stone_Game_V.java
+++ 3. Hard/1563_Stone_Game_V.java
@@ -1,6 +1,6 @@
 class Solution {
     int prefixSum[];
-    int dp[][]; //memoization
+    int dp[][]; //memoization array
     public int stoneGameV(int[] stoneValue) {
         int n = stoneValue.length;
         prefixSum = new int[n];
