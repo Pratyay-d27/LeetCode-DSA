0c4770806005c677a30b87ca0f0b27fc7d8bd5df
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-08-30 23:31:09 +0530

    Implement maximum69Number function in Solution class

---
+++ Greedy/1323_Maximum_69_Number.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maximum69Number (int num) {
+        if(num == 9 || num == 99 || num == 999 || num == 9999)
+        return num;
+
+        ArrayList<Integer> list = new ArrayList<>();
+        while(num > 0)
+        {
+            list.add(num % 10);
+            num /= 10;
+        }
+        int i = 0;
+        for(i = list.size()-1; i>=0; i--)
+        {
+            if(list.get(i) == 6)
+            break;
+        }
+        list.set(i, 9);
+        int ans = 0;
+        for(int j = list.size()-1; j>=0; j--)
+        {
+            ans = ans * 10 + list.get(j);
+        }
+        return ans;
+    }
+}
