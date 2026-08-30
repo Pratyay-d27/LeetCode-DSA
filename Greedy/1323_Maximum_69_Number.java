a7347d5abcc46931c25c8d684b5a6a8de1223761
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-08-30 23:31:49 +0530

    Update 1323_Maximum_69_Number.java

--- Greedy/1323_Maximum_69_Number.java
+++ Greedy/1323_Maximum_69_Number.java
@@ -1,4 +1,4 @@
-// Solution without converting 
+// Solution without converting to 
 class Solution {
     public int maximum69Number (int num) {
         if(num == 9 || num == 99 || num == 999 || num == 9999)
