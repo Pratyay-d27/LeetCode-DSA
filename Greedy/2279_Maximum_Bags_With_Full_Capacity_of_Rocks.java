5699ccf10103a3b5c4dbca83d5886413d3cd2045
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-08-31 16:43:38 +0530

    Create 2279_Maximum_Bags_With_Full_Capacity_of_Rocks.java

---
+++ Greedy/2279_Maximum_Bags_With_Full_Capacity_of_Rocks.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maximumBags(int[] capacity, int[] rocks, int additionalRocks) {
+        for(int i = 0; i<rocks.length; i++)
+        {
+            rocks[i] = capacity[i] - rocks[i];
+        }
+        Arrays.sort(rocks);
+        int max = 0;
+        for(int i = 0; i<rocks.length; i++)
+        {
+            if(rocks[i] == 0)
+            max++;
+
+            else if(rocks[i] != 0 && additionalRocks >= rocks[i] )
+            {
+                additionalRocks -= rocks[i];
+                rocks[i] = 0;
+                max++;
+            }
+            if(rocks[i] > additionalRocks)
+            break;
+        }
+        return max;
+    }
+}
