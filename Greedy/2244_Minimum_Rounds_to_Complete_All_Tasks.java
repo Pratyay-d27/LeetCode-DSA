e232f3bb524a5e18ca476501691780272cc2a914
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-09-01 14:16:39 +0530

    Update 2244_Minimum_Rounds_to_Complete_All_Tasks.java

--- Greedy/2244_Minimum_Rounds_to_Complete_All_Tasks.java
+++ Greedy/2244_Minimum_Rounds_to_Complete_All_Tasks.java
@@ -12,7 +12,7 @@ class Solution {
         }
 
         int rounds = 0;
-        for(var ele: map.entrySet()) // traversing the m
+        for(var ele: map.entrySet()) // traversing the ma
         {
             int value = ele.getValue();
 
