839ee3c7b6a4a358bb7a0d20ca2cbc8a3838f7e3
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-09-02 19:02:37 +0530

    Update 5_Longest_Palindromic_Substring.java

--- DP/5_Longest_Palindromic_Substring.java
+++ DP/5_Longest_Palindromic_Substring.java
@@ -27,7 +27,7 @@ class Solution {
         if(i>j)
         return 1;
 
-        if(dp[i][j] != -1)
+        if(dp[i][j] != -1) //
         return dp[i][j];
 
         if(str.charAt(i) == str.charAt(j))
