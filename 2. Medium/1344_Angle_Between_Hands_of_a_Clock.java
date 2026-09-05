8e68f536bb48bc4fd624e4d279e3fefafc367152
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-09-05 15:30:16 +0530

    Update 1344_Angle_Between_Hands_of_a_Clock.java

--- 2. Medium/1344_Angle_Between_Hands_of_a_Clock.java
+++ 2. Medium/1344_Angle_Between_Hands_of_a_Clock.java
@@ -1,6 +1,6 @@
 class Solution {
     public double angleClock(int hour, int minutes) {
-        //formula based 
+        //formula based solutio
         double m = minutes * 6;
         double h = 30 * (hour + (minutes/60.0));
         double diff = Math.abs(m - h);
