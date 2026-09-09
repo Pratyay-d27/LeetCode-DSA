28a0645577f7ec7c00f5fdac19057b05d4493179
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-09-09 23:22:19 +0530

    Create 3871_Count_Commas_in_Range_II.java

---
+++ 2. Medium/3871_Count_Commas_in_Range_II.java
@@ -0,0 +1,25 @@
+class Solution {
+    public long countCommas(long n) {
+        if(n <= 999)
+        return 0;
+        
+        long count = 0;
+        long lower = 1000;
+        long comma = 1;
+        while(lower <= n)
+        {
+            long upper = lower*1000 - 1;
+            if(upper >= n)
+            upper = n;
+
+            long nos = upper - lower + 1;
+            count += nos*comma;
+
+            lower *= 1000;
+            comma += 1;
+        }
+        return count;
+    }
+}
+
+
