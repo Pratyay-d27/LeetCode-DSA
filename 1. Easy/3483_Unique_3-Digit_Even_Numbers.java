97d466216f584794c30e865e3040f830475a36ba
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-09-11 16:55:38 +0530

    Create 3483_Unique_3-Digit_Even_Numbers.java

---
+++ 1. Easy/3483_Unique_3-Digit_Even_Numbers.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int totalNumbers(int[] digits) {
+        HashSet<Integer> set = new HashSet<>();
+        int n = digits.length;
+        int count = 0;
+        for(int i = 0; i<n; i++)
+        {
+            for(int j = 0; j<n; j++)
+            {
+                for(int k = 0; k<n; k++)
+                {
+                    int a = digits[i];
+                    int b = digits[j];
+                    int c = digits[k];
+                    if(i != j && j != k && i != k)
+                    {
+                        int num = a*100 + b * 10 + c;
+                        if(num >= 100 && num <= 999 && num % 2 == 0 && set.contains(num) == false)
+                        {
+                            count++;
+                            set.add(num);
+                        }
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
