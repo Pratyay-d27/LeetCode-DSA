db9ddd022d56be99536ad225d61265fcbc656587
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-09-11 16:56:45 +0530

    Update 3483_Unique_3-Digit_Even_Numbers.java

--- 1. Easy/3483_Unique_3-Digit_Even_Numbers.java
+++ 1. Easy/3483_Unique_3-Digit_Even_Numbers.java
@@ -1,6 +1,6 @@
 class Solution {
     public int totalNumbers(int[] digits) {
-        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> set = new HashSet<>(); // 
         int n = digits.length;
         int count = 0;
         for(int i = 0; i<n; i++)
