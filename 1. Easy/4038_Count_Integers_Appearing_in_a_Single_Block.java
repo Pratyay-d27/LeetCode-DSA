d100ee54c50564394cc6b3f1d56460b5783fa9b9
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-09-22 16:39:59 +0530

    Update 4038_Count_Integers_Appearing_in_a_Single_Block.java

--- 1. Easy/4038_Count_Integers_Appearing_in_a_Single_Block.java
+++ 1. Easy/4038_Count_Integers_Appearing_in_a_Single_Block.java
@@ -1,4 +1,4 @@
-// Here HashMap increases time complexity as the input constraints are small so NOT 
+// Here HashMap increases time complexity as the input constraints are small so NOT using 
 class Solution {
     public int countSpecialIntegers(int[] nums) {
         int check[] = new int[101];
