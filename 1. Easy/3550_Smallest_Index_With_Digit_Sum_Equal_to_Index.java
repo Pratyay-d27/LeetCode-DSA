0f1521703e2fd8b614b367603c84ae0d1129d14d
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-09-24 13:40:56 +0530

    Add solution for smallest index with digit sum

---
+++ 1. Easy/3550_Smallest_Index_With_Digit_Sum_Equal_to_Index.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int smallestIndex(int[] nums) {
+        for(int i = 0; i<nums.length; i++)
+        {
+            int key = nums[i], sum = 0;
+            while(key > 0)
+            {
+                int d = key % 10;
+                sum += d;
+                key = key/10;
+            }
+            if(sum == i)
+            return i;
+        }
+        return -1;
+    }
+}
