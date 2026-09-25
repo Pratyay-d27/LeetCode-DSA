fa440a3223d9f4a404878cb15dc50decd1ee9427
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-09-25 17:55:36 +0530

    Update 748_Shortest_Completing_Word.java

--- 1. Easy/748_Shortest_Completing_Word.java
+++ 1. Easy/748_Shortest_Completing_Word.java
@@ -2,7 +2,7 @@
 
 class Solution {
     public String shortestCompletingWord(String licensePlate, String[] words) {
-        ArrayList<String> list = new ArrayList<>(); //Declaring ArrayList to store the strings satisfying 
+        ArrayList<String> list = new ArrayList<>(); //Declaring ArrayList to store the strings satisfying the 
         int minLength = Integer.MAX_VALUE;
 
         String str = licensePlate.toLowerCase();
