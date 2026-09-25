74f1c2fc1ae90bd566b0e4bf67b1282cdb4b5c67
Pratyay-d27 <pratyay.ghose@gmail.com>
2026-09-25 17:53:49 +0530

    Create 748_Shortest_Completing_Word.java

---
+++ 1. Easy/748_Shortest_Completing_Word.java
@@ -0,0 +1,42 @@
+class Solution {
+    public String shortestCompletingWord(String licensePlate, String[] words) {
+        ArrayList<String> list = new ArrayList<>();
+        int minLength = Integer.MAX_VALUE;
+
+        String str = licensePlate.toLowerCase();
+        int keyFreq[] = new int[26];
+        for(char ele: str.toCharArray())
+        if(ele >= 'a' && ele <= 'z')
+        keyFreq[ele - 'a']++;
+
+        for(String ele: words)
+        {
+            int freq[] = new int[26];
+
+            for(char ch: ele.toCharArray())
+            if(ch >= 'a' && ch <= 'z')
+            freq[ch - 'a']++;
+
+            boolean match = true;
+            for(int i = 0; i<26; i++)
+            {
+                if(keyFreq[i] != 0 && keyFreq[i] - freq[i] > 0)
+                {
+                    match = false;
+                    break;
+                }
+            }
+            if(match == true)
+            {
+                list.add(ele);
+                minLength = Math.min(minLength, ele.length());
+            }
+        }
+
+        for(String ele: list)
+        if(ele.length() == minLength)
+        return ele;
+
+        return "";
+    }
+}
